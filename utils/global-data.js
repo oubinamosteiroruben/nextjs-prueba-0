5728637e39230ca1c219e87e2bbb55e83f7c61ec
oubinamosteiroruben@gmail.com <oubinamosteiroruben@gmail.com>
2024-03-08 15:05:05 +0100

    Prueba cambio de literales

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,7 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+  const name = process.env.BLOG_NAME ? decodeURI(process.env.BLOG_NAME) : 'Rubén Oubiña';
+  const blogTitle = process.env.BLOG_TITLE  ? decodeURI(process.env.BLOG_TITLE) : 'Esto es una prueba de edición';
+  const footerText = process.env.BLOG_FOOTER_TEXT    ? decodeURI(process.env.BLOG_FOOTER_TEXT) : 'All rights reserved.';
 
   return {
     name,
